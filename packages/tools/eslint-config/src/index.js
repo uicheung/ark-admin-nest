4720a0e25994c9a855bf5a15076eda1f5c53967f
小小源逼 <qa894178522@qq.com>
2022-12-15 00:05:18 +0800

    feat: remove ignorePatterns config

--- packages/tools/eslint-config/src/index.js
+++ packages/tools/eslint-config/src/index.js
@@ -11,7 +11,6 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'node_modules', 'dist', 'webpack-hmr.config.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
